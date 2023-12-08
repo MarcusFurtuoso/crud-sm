af8eea64121a3d14d28c9350fb4b28f8981dca69
MarcusFurtuoso <marcusvini678@gmail.com>
2023-12-08 18:50:11 -0300

    add model form

---
+++ src/app/model/forms/edit-user.form.ts
@@ -0,0 +1,11 @@
+export class EditUserform {
+  id: number;
+  name: string;
+  job: string;
+
+  constructor () {
+    this.id = 0;
+    this.name = '';
+    this.job = '';
+  }
+}
---
+++ src/app/model/forms/register.form.ts
@@ -0,0 +1,9 @@
+export class RegisterForm {
+  email: string;
+  password: string;
+
+  constructor() {
+    this.email = '';
+    this.password = '';
+  }
+}
