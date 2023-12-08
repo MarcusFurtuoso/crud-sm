b5e1cbdceee1e50ed0c9937baa2ecafe713e33fd
MarcusFurtuoso <marcusvini678@gmail.com>
2023-12-08 18:52:56 -0300

    add auth routing and module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+  ],
+})
+export class AuthModule {}
